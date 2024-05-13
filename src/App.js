f56dde45f52a563c39e8aa3aa35f15ff37b82f4c
Students <linhtom2012@gmai.com>
2024-05-13 20:44:55 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ 
+ 
+  
+  
+  return (
+    <center>
+
+    <h1> Duc dep trai gap {2*5} lan tien</h1>
+
+    </center>
+  );
+}
+
+export default App;
